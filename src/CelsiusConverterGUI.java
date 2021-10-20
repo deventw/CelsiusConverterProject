3defce600cddbbfcfd6108c8e49a7a65c3029360
KWONGTAKWAI <kwongtakwai98@gmail.com>
2021-10-20 11:03:13 +0800

    First Push.

---
+++ src/CelsiusConverterGUI.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.InputMethodListener;
+
+public class CelsiusConverterGUI extends JFrame{
+    private JPanel mainPanel;
+    private JTextField celsiusTextField;
+    private JLabel ceisiusLabel;
+    private JButton convertButton;
+    private JLabel fahrenheitLabel;
+
+    public CelsiusConverterGUI(String title){
+        super(title);
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(mainPanel);
+        this.pack();
+        convertButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //get text from text field and convert to double and update temperature
+                int tempFah = (int)((Double.parseDouble(celsiusTextField.getText()))*1.8+32);
+                fahrenheitLabel.setText("Fahrenheit 華氏溫度："+tempFah);
+            }
+        });
+    }
+
+    public static void main(String[] arg){
+        JFrame frame = new CelsiusConverterGUI("My Celsius Converter");
+        frame.setVisible(true);
+    }
+}
